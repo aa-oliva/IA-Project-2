22c4637bcce788c070406b29458d6662bb08a36a
Andres Oliva <oli12149@uvg.edu.gt>
2018-04-08 16:09:46 -0600

    Open txt files and rewrite the content
    Open txt files and rewrite the content in matrix with ham and spam labels

--- IA Project 2/task1/functions.py
+++ IA Project 2/task1/functions.py
@@ -5,3 +5,27 @@ Created on Sun Apr  8 14:34:01 2018
 @author: aa-ol
 """
 
+import random, re
+
+#Funcion para que todas las letras sean minusculas11
+def sanitizar(cadena_entrada):
+    return ((re.sub('[^A-Za-z0-9 \t]+', '', cadena_entrada)).lower())
+
+#Funcion para abrir archivos txt y guardarlos linea por linea
+def getText(file_name):
+    matriz = []
+    data = [sanitizar(line.strip()) for line in open(file_name, 'r', encoding="utf8")]
+    
+    for i in range (len(data)):
+        line = []
+        x,y = data[i].split("\t")
+        line.append(x)
+        line.append(y)
+        matriz.append(line)
+    return matriz
+
+def trainingSample(matriz, percentage):
+    training = []
+    for _ in range (0, int((len (matriz))*percentage)):
+        training.append(matriz.pop(random.randrange(0, len(matriz))))
+        return training
--- IA Project 2/task1/main.py
+++ IA Project 2/task1/main.py
@@ -5,3 +5,17 @@ Created on Sun Apr  8 14:33:08 2018
 @author: aa-ol
 """
 
+import functions as fc
+
+def main ():
+    matriz = fc.getText("corpus.txt")
+    training = fc.trainingSample(matriz, .8)
+    test = fc.trainingSample(matriz, .5)
+    cross_validation = fc.trainingSample(matriz, 1)
+    
+    print ("El tamaño de las oraciones con etiqueta es de :", str(len(matriz)))
+    print ("Tamañ 0.8 de los datos es de:", str(int(len(matriz)*.8)), "\n")
+    
+    
+    
+main()
