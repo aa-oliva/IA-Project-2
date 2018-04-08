ec9153df329b46eb2a20cae15c818a713c1860ff
Andres Oliva <oli12149@uvg.edu.gt>
2018-04-08 14:36:15 -0600

    Se empieza con el primer task

---
+++ IA Project 2/task1/functions.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  8 14:34:01 2018
+
+@author: aa-ol
+"""
+
---
+++ IA Project 2/task1/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  8 14:33:08 2018
+
+@author: aa-ol
+"""
+
