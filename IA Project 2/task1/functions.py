c8aaf79450db13a29cde27e7ce7614b4f05a3faa
Andres Oliva <oli12149@uvg.edu.gt>
2018-04-09 09:11:58 -0600

    Se procedio a mejorar el rendimiento

--- IA Project 2/task1/functions.py
+++ IA Project 2/task1/functions.py
@@ -11,29 +11,41 @@ import random, re
 def sanitizar(string_input):
     return ((re.sub('[^A-Za-z0-9 \t]+', '', string_input)).lower())
 
+# Function to write a txt file
+def writeOutput(matrix, name):
+    file = open(name, "w")
+    for i in range(len(matrix)):
+        Label = matrix[i]
+        file.write(str(Label[0])+" \t"+str(Label[1])+"\n")  
+    file.close()
+
 # Function to open txt files and save them line by line
-def getText(file_name):
+def getText(file_name, its_entry):
     matrix = []
     data = [sanitizar(line.strip()) for line in open(file_name, 'r', encoding="utf8")]
     
     for i in range (len(data)):
         line = []
-        x,y = data[i].split("\t")
-        line.append(x)
-        line.append(y)
+        if (its_entry):
+            phrase = data[i]
+            line.append(phrase)
+        else:
+            x,y = data[i].split("\t")
+            line.append(x)
+            line.append(y)
         
         matrix.append(line)
     return matrix
 
 # Function to separate the data by percentage and  obtain a sample
-def trainingSample(matrix, percentage):
+def sampleF(matrix, percentage):
     training_sample = []
     for _ in range (0, int((len (matrix))*percentage)):
         training_sample.append(matrix.pop(random.randrange(0, len(matrix))))
     return training_sample
 
 # Function to classify what type of data is per line according to flag
-def classifyMessages(matrix):
+def classify(matrix):
     spam = []
     ham = []
     for i in range(len(matrix)):
@@ -46,7 +58,7 @@ def classifyMessages(matrix):
     return spam, ham
 
 # Function to obtain the unique words of each list
-def getUniqueWords(array):
+def uniqueF(array):
     unique = []
     for i in range(len(array)):
         phrase = array[i].split(" ")
@@ -55,13 +67,7 @@ def getUniqueWords(array):
                 unique.append(word)
     return unique
 
-# Function to obtain only the list of a column
-def getColumn(matrix, column_num):
-    column = []
-    for i in range(len(matrix)):
-        label = matrix[i]
-        column.append(label[column_num])
-    return column 
+
 
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
@@ -72,19 +78,23 @@ def getColumn(matrix, column_num):
 INPUT: int K, string palabra, array tipo_palabra, int palabras_totales
 """
 def laplaceSmoothingBasic(k, palabra, tipo_palabra, palabras_totales):
+    res = 0.0
     cant_ocurrencia = 0.0
     cant_observaciones = len(tipo_palabra)
     for i in range(len(tipo_palabra)):
         if tipo_palabra[i]==palabra: cant_ocurrencia +=1
-    return ((cant_ocurrencia + k)/(cant_observaciones + k*palabras_totales))
+    res = (cant_ocurrencia+k)/(cant_observaciones+k*palabras_totales)
+    return (res)
 
 """
 INPUT: array string mensajes, int k, array tipo_palabra, int palabras_totales
 """
 def probabilidadOracion(mensaje, k, tipo_palabra, palabras_totales):
     probabilidad = 1.0
+    
     for i in range(len(mensaje)):
-        probabilidad =  probabilidad * laplaceSmoothingBasic(k, mensaje[i], tipo_palabra, palabras_totales)
+        laplace = laplaceSmoothingBasic(k, mensaje[i], tipo_palabra, palabras_totales)
+        probabilidad =  probabilidad * laplace
     return probabilidad
 
 """
@@ -111,23 +121,9 @@ def propabilidadTotal(mensaje, k, objetivo, tipo_ham, tipo_spam, palabras_totale
     if (objetivo=="spam"): return ((parte1)/(parte1 + parte2))
     elif(objetivo=="ham"): return ((parte2)/(parte2 + parte1))
     
-def readEntryFile(file_name, sanit=True, log=False, split=False):
-    matriz = []
-    data = [sanitizar(line.strip()) for line in open(file_name, 'r')]
-    for i in range(len(data)):
-        columna = []
-        if(split): phrase = data[i].split(" ")
-        else: phrase = data[i]
-        columna.append(phrase)
-        matriz.append(columna)
-    
-    if (log):
-        for j in range(len(data)):
-            print(matriz[j])
-        print("\n")
-    return matriz
 
-def generateOutputMatrix(input_matrix, i_actual, ham_words, spam_words, len_total_words_training, training_group, log=False ):
+
+def generateOutputMatrix(input_matrix, i_actual, ham_words, spam_words, len_total_words_training, training_group):
     salida = []
     for i in range(len(input_matrix)):
         tupla_salida = []
@@ -137,7 +133,6 @@ def generateOutputMatrix(input_matrix, i_actual, ham_words, spam_words, len_tota
         elif(len(tupla)==1): phrase = sanitizar(tupla[0])
         phrase_words = phrase.split(" ")
         
-        if(log): print ("i: ", i_actual," iter: ", i, ": ", phrase)
         
         prob_spam = propabilidadTotal(phrase_words, i_actual, "spam", ham_words, spam_words, len_total_words_training, training_group)
         prob_ham = propabilidadTotal(phrase_words, i_actual, "ham", ham_words, spam_words, len_total_words_training, training_group) 
@@ -151,9 +146,6 @@ def generateOutputMatrix(input_matrix, i_actual, ham_words, spam_words, len_tota
     return salida
             
 def getAccuracy(matrix_to_evaluate, base_matrix, log=False):
-    #if(len(matrix_to_evaluate)!=len(base_matrix)):
-    #    print("MATRICES DIFERENTES LEN")
-    #else:
         aciertos = 0
         for i in range(len(matrix_to_evaluate)):
             tupla_to_evaluate = matrix_to_evaluate[i]
@@ -164,9 +156,3 @@ def getAccuracy(matrix_to_evaluate, base_matrix, log=False):
         return (aciertos/len(matrix_to_evaluate))
  
 
-def writeOutput(matrix, name):
-    file = open(name, "w")
-    for i in range(len(matrix)):
-        tupla = matrix[i]
-        file.write(str(tupla[0])+" \t"+str(tupla[1])+"\n")  
-    file.close()
--- IA Project 2/task1/main.py
+++ IA Project 2/task1/main.py
@@ -8,46 +8,48 @@ Created on Sun Apr  8 14:33:08 2018
 import functions as funct
 
 def main ():
+    best_k, best_accuracy = 0, 0.0
     # Obtaining and growing vareables to perform training and tests
     # The data is stored in a matrix to obtain the information separated by line and labeled
-    matrix = funct.getText("corpus.txt")
+    matrix = funct.getText("corpus.txt", False)
     
     # It is decided to have a sample of 80% and 50% of the data that was previously obtained
-    training = funct.trainingSample(matrix, .8)
-    test = funct.trainingSample(matrix, .5)
+    training = funct.sampleF(matrix, .8)
+    test = funct.sampleF(matrix, .5)
     
     # Cross validation is obtained
-    cross_validation = funct.trainingSample(matrix, 1)
+    cross_validation = funct.sampleF(matrix, 1)
     
     # The data is classified according to the label and the number of words per label is calculated
-    spam, ham = funct.classifyMessages(training)
-    spam_words = funct.getUniqueWords(spam)
-    ham_words = funct.getUniqueWords(ham)
+    spam, ham = funct.classify(training)
+    spam_words = funct.uniqueF(spam)
+    ham_words = funct.uniqueF(ham)
     
     # The total number of words used for training is calculated 
-    total_words_training = funct.getUniqueWords(funct.getColumn(training, 1))
-    
-    # The results are printed  
-    print ("El tamaño de las oraciones con etiqueta es de :", str(len(matrix)))
-    print ("Tamañ 0.8 de los datos es de:", str(int(len(matrix)*.8)), "\n")
+    column = []
+    for i in range(len(matrix)):
+        label = training[i]
+        column.append(label[1])
+    total_words_training = funct.uniqueF(column)
+  
     
     # Initialize test
-    best_i, best_accuracy = 0, 0.0
     
     for i in range(1,10):
-        salida_cross = funct.generateOutputMatrix(cross_validation, i, ham_words, spam_words, len(total_words_training), training, True)
+        salida_cross = funct.generateOutputMatrix(cross_validation, i, ham_words, spam_words, len(total_words_training), training)
         accuracy = funct.getAccuracy(salida_cross, cross_validation)
-        if (accuracy > best_accuracy): best_accuracy, best_i = accuracy, i
-    print("\nCROSS VALIADTION: Mejor i y accuracy: ", best_i, ": ", best_accuracy)
+        if (accuracy > best_accuracy): best_accuracy, best_k = accuracy, i
+    print("\nCROSS VALIADTION: Best K: ", best_k, " | Accuracy: ", best_accuracy)
     
     #--Test Phase---
-    salida_test = funct.generateOutputMatrix(test, best_i, ham_words, spam_words, len(total_words_training), training, True)
+    salida_test = funct.generateOutputMatrix(test, best_k, ham_words, spam_words, len(total_words_training), training)
     test_acc = funct.getAccuracy(salida_test, test)
-    print("\nTEST: i y accuracy: ", best_i, ": ", test_acc, "\n")
+    print("\nTEST: Best K: ", best_k,   " | Accuracy: ", test_acc, "\n")
     
     #--Input Phase---
-    input_file = funct.readEntryFile("prueba.txt", True, False, False)
-    salida_input = funct.generateOutputMatrix(input_file, best_i, ham_words, spam_words, len(total_words_training), training, True)
+    input_file = funct.getText("prueba.txt",True)
+    salida_input = funct.generateOutputMatrix(input_file, best_k, ham_words, spam_words, len(total_words_training), training)
     funct.writeOutput(salida_input, "output.txt")
     
+
 main()
